0615ce6848f0b5d66b795c659798a1f0af08c986
anas-raza~ <anusraza268@gmail.com>
2024-07-02 07:07:49 -0700

    modified

---
+++ app.js
@@ -0,0 +1,11 @@
+const container_card=document.querySelector('.container_card')
+const card=document.querySelector('.card')
+console.log(container_card);
+
+
+function submit() {
+    console.log(container_card.value);
+    console.log(card.value);
+    
+    
+}
